18593002afd55131666feb288d34fddaad823d23
paolaperdomo <paolaperdomo079@gmail.com>
2024-07-03 16:58:17 +0200

    Mapa en modo claro

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -12,13 +12,21 @@ const Map = () => {
           className="full-height-map"
           center={[40.463667, -3.74922]}
           zoom={6}
-          minZoom={3}
-          maxZoom={19}
+          minZoom={7}
+          maxZoom={14}
           scrollWheelZoom={true}>
             <TileLayer
              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
            />
+           <Marker
+              key="1"
+              position={[40.463667, -3.74922]}>
+              <Popup>
+                Spain
+                <br />
+              </Popup>
+            </Marker>
         </MapContainer>
   )
 }
