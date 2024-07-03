08a10b0d4038d573bde2e929ad511b42b1c7bfe1
indiakka <78439130+indiakka@user.noreply.github.com>
2024-07-03 15:18:31 +0200

    crear estructura de carpetas

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/router/index.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout.jsx";
+import Home from "../pages/home/Home.jsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
