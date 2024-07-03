00dc84667ad53127b63faaad4979fde1ff535c77
paolaperdomo <paolaperdomo079@gmail.com>
2024-07-03 16:38:33 +0200

    Mapa renderizado

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import 'leaflet/dist/leaflet.css'
+import {MapContainer, Marker, Popup} from 'react-leaflet'
+import L from 'leaflet'
+import { TileLayer } from 'react-leaflet'
+import './map.css'
+
 
 const Map = () => {
   return (
-    <div>
-      
-    </div>
+        <MapContainer
+          className="full-height-map"
+          center={[40.463667, -3.74922]}
+          zoom={6}
+          minZoom={3}
+          maxZoom={19}
+          scrollWheelZoom={true}>
+            <TileLayer
+             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+           />
+        </MapContainer>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Map from "../components/map/Map";
 
 const Home = () => {
   return (
     <div>
-      
+      <Map />
     </div>
   )
 }
 
-export default Homes
+export default Home
 
