fe7f950b11d332a452964d7610c8f196960712ee
IsaLagu <isabelafonso321@hotmail.com>
2024-07-04 21:01:12 +0200

    añade eliminar incidencia

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -8,17 +8,24 @@ const Profile = () => {
     intensidad: "",
   });
   const [showNotification, setShowNotification] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const { temperatura, tamaño } = formData;
 
     setShowNotification(true);
   };
+
+  const handleEditButtonClick = () => {
+    setShowEditForm(true);
+  };
+
   return (
     <div className="wrapper-forms">
       <div className="form-container">
@@ -57,7 +64,7 @@ const Profile = () => {
           <input className="form-input" type="tel" name="telefono" id="phone" placeholder="Teléfono" />
 
           <button className="sign-buttons" id="blue-button-sign">
-            Enviar
+            Editar
           </button>
         </form>
       </div>
@@ -65,50 +72,150 @@ const Profile = () => {
         <form className="reg-form" onSubmit={handleSubmit}>
           <div className="reg-form-container">
             <h1 className="reg-title">Modificar incendio registrado</h1>
-            <h3 className="reg-subtitle">Rellene los siguientes campos:</h3>
-            <h2 className="form-subtitles">Temperatura y tamaño del foco*</h2>
-            <div className="reg-container reg-suffix">
-              <input
-                type="number"
-                name="temperatura"
-                className="input-reg input-data"
-                placeholder="Temperatura"
-                value={formData.temperatura}
-                onChange={handleChange}
-              />
-              <span className="input-suffix">°C</span>
-              <input
-                type="number"
-                name="tamaño"
-                className="input-reg input-data"
-                placeholder="Tamaño*"
-                value={formData.tamaño}
-                onChange={handleChange}
-              />
-              <span className="input-suffix">km²</span>
-            </div>
-            <h2 className="form-subtitles">Intensidad</h2>
-            <div className="reg-container reg-suffix">
-              <input
-                type="text"
-                name="intensidad"
-                className="input-reg input-data"
-                placeholder="Intensidad"
-                value={formData.intensidad}
-                onChange={handleChange}
-              />
-            </div>
-            {showNotification && (
-              <div className="notification">
-                <article>Agradecemos su colaboración. Gracias por registrar un nuevo foco.</article>
+            <strong>Selecciona editar o eliminar:</strong>
+            <div className="reported-fire-container">
+              <p>Incidencia nº91989</p>
+              <div className="wrapper-buttons">
+                <svg
+                  className="delete-button"
+                  width="25px"
+                  height="25px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      d="M10 11V17"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                    <path
+                      d="M14 11V17"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                    <path
+                      d="M4 7H20"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                    <path
+                      d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                    <path
+                      d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </g>
+                </svg>
+                <svg
+                  className="edit-button"
+                  onClick={handleEditButtonClick}
+                  width="20px"
+                  height="20px"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#000000"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <title></title>
+                    <g id="Complete">
+                      <g id="edit">
+                        <g>
+                          <path
+                            d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
+                            fill="none"
+                            stroke="#000000"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                          ></path>
+                          <polygon
+                            fill="none"
+                            points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
+                            stroke="#000000"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                          ></polygon>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
               </div>
-            )}
+            </div>
           </div>
-          <button className="reg-button" id="blue-button" type="submit">
-            Enviar
-          </button>
         </form>
       </div>
+      {showEditForm && (
+        <div className="form-container form-edit-fire">
+          <form className="reg-form" onSubmit={handleSubmit}>
+            <div className="reg-form-container">
+              <h1 className="reg-title">Modificar incendio registrado</h1>
+              <h3 className="reg-subtitle">Rellene los siguientes campos:</h3>
+              <h2 className="form-subtitles">Temperatura y tamaño del foco*</h2>
+              <div className="reg-container reg-suffix">
+                <input
+                  type="number"
+                  name="temperatura"
+                  className="input-reg input-data"
+                  placeholder="Temperatura"
+                  value={formData.temperatura}
+                  onChange={handleChange}
+                />
+                <span className="input-suffix">°C</span>
+                <input
+                  type="number"
+                  name="tamaño"
+                  className="input-reg input-data"
+                  placeholder="Tamaño*"
+                  value={formData.tamaño}
+                  onChange={handleChange}
+                />
+                <span className="input-suffix">km²</span>
+              </div>
+              <h2 className="form-subtitles">Intensidad</h2>
+              <div className="reg-container reg-suffix">
+                <input
+                  type="text"
+                  name="intensidad"
+                  className="input-reg input-data"
+                  placeholder="Intensidad"
+                  value={formData.intensidad}
+                  onChange={handleChange}
+                />
+              </div>
+              {showNotification && (
+                <div className="notification">
+                  <article>Agradecemos su colaboración. Gracias por registrar un nuevo foco.</article>
+                </div>
+              )}
+            </div>
+            <button className="reg-button" id="blue-button" type="submit">
+              Editar
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
