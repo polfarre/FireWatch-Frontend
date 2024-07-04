dee89f614248536461264a34474b20ef20bcde26
polfarre <pol.farre@basetis.com>
2024-07-04 22:44:17 +0200

    mapa listo

--- src/pages/registro/Registro.jsx
+++ src/pages/registro/Registro.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import "./registro.css";
 
 const Registro = () => {
